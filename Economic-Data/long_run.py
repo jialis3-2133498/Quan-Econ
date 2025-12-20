e979deb0467d11ccf3925234e6d740492e4a1468
Jiali Sheng <jerry_lovae@Jialis-MacBook-Air.local>
2025-12-19 16:02:56 -0800

    Rearrange files

--- Economic-Data/long_run.py
+++ Economic-Data/long_run.py
@@ -138,9 +138,78 @@ def draw_events(events, ax):
         event_mid = sum(event.year_range)/2
         ax.text(event_mid,
                 event.y_text, event.text,
-                color = event.color, **t_params)
+                color=event.color, **t_params)
         ax.axvspan(*event.year_range, color=event.color, alpha=0.2)
         ax.axvline(event_mid, ymin=0, ymax=1, color=event.color,
                    alpha=0.15)
 draw_events(events, ax2)
+
+# China
+fig3, ax3 = plt.subplots(dpi=100, figsize=(10, 6))
+country = ['CHN']
+draw_interp_plots(gdp_pc[country].loc[1500:2000],
+                  country,
+                  'international dollars', 'year',
+                  color_mapping, code_to_name, 2, True, ax3)
+ylim = ax3.get_ylim()[1]
+events2 = [
+    Event((1655, 1684), ylim*0.6,
+          'Closed-door Policy\n(1655-1684)',
+          'tab:orange', 1),
+    Event((1760, 1840), ylim*0.4, 
+        'Industrial Revolution\n(1760-1840)', 
+        'grey', 1),
+    Event((1839, 1842), ylim*0.5, 
+        'First Opium War\n(1839–1842)', 
+        'tab:red', 1.07),
+    Event((1861, 1895), ylim*0.9, 
+        'Self-Strengthening Movement\n(1861–1895)', 
+        'tab:blue', 1.14),
+    Event((1939, 1945), ylim*0.6, 
+        'WW 2\n(1939-1945)', 
+        'tab:red', 1),
+    Event((1948, 1950), ylim*0.5, 
+        'Founding of PRC\n(1949)', 
+        color_mapping['CHN'], 1.08),
+    Event((1958, 1962), ylim*0.4, 
+        'Great Leap Forward\n(1958-1962)', 
+        'tab:orange', 1.18),
+    Event((1978, 1979), ylim*0.7, 
+        'Reform and Opening-up\n(1978-1979)', 
+        'tab:blue', 1.24)
+]
+draw_events(events2, ax3)
+
+# US and UK
+fig4, ax4 = plt.subplots(dpi=100, figsize=(10, 6))
+country = ['GBR', "USA"]
+ylim = ax4.get_ylim()[1]
+draw_interp_plots(gdp_pc[country].loc[1500:2000],
+                  country,
+                  'international dollars', 'year',
+                  color_mapping, code_to_name, 2, False, ax4)
+events3 = [
+    Event((1651, 1651), ylim* 0.5,
+          'Navigation Ack(UK)\n(1651)',
+          'tab:yellow', 1.1),
+    Event((1760, 1840), ylim*0.6,
+          'Industrial Revolution\n(1760-1840)',
+          'tab:grey', 1.1),
+    Event((1765, 1791), ylim*0.7,
+          'American Revolution\n(1765-1791)',
+          color_mapping['USA'], 1.2),
+    Event((1849, 1849), ylim*0.8,
+          'Repeal of Nagigation Act(UK)\n(1849)',
+          color_mapping['GBR'], 1.2),
+    Event((1861, 1865), ylim*0.7,
+          'American Civil War\n(1861-1865)',
+          color_mapping['USA'], 1.2),
+    Event((1914, 1918), ylim*0.4,
+          'WW1\n(1914-1918)',
+          'tap:red', 1.2),
+    Event((1939, 1945), ylim*0.6,
+          'WW2'),
+    Event()
+]
+
 plt.show()
